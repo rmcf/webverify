0fcad170e2d291fa3e9303e40fb2a024c64a17ed
avm <rmcf@ukr.net>
2021-03-11 23:20:51 +0200

    app mockup

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,96 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header elevated>
+      <!-- toolbar -->
+      <q-toolbar class="bg-white text-blue-grey-8">
+        <q-avatar>
+          <img src="/img/logo.svg" />
+        </q-avatar>
+
+        <q-toolbar-title>
+          WebVerify
+        </q-toolbar-title>
+
+        <q-btn flat round dense icon="info_outline" />
+      </q-toolbar>
+    </q-header>
+
+    <!-- drawer -->
+    <q-drawer
+      v-model="leftDrawerOpen"
+      v-if="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header class="text-grey-8">
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <!-- banner -->
+    <q-parallax :height="400" src="/img/banner.jpg">
+      <div class="flex flex-center">
+        <h2 class="text-white banner-text">
+          Digital Signature Verification
+        </h2>
+      </div>
+    </q-parallax>
+
+    <!-- page container -->
+    <q-page-container class="page-container">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksData = [
+  {
+    title: "Login",
+    caption: "Login into system",
+    icon: "school",
+    link: "#"
+  },
+  {
+    title: "Verify",
+    caption: "Digital Signature Verification",
+    icon: "code",
+    link: "#"
+  }
+];
+
+export default {
+  name: "MainLayout",
+  components: { EssentialLink },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    };
+  }
+};
+</script>
+
+<style scoped>
+.q-toolbar .q-avatar {
+  border-radius: 0;
+}
+
+.page-container {
+  padding-left: 10rem;
+  padding-right: 10rem;
+}
+
+.banner-text {
+  text-shadow: 2px 2px 5px #000;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,128 @@
+<template>
+  <q-page>
+    <div class="q-gutter-md q-pa-sm">
+      <q-card class="my-card">
+        <q-card-section>
+          <div>
+            <q-uploader
+              label="Upload files"
+              color="amber"
+              text-color="black"
+              multiple
+              batch
+              flat
+              no-thumbnails
+              style="width: 100%"
+            />
+          </div>
+          <div class="row justify-end">
+            <q-btn
+              class="q-ml-sm q-mt-md"
+              label="Verify"
+              type="submit"
+              color="primary"
+            />
+            <q-btn
+              label="Remove all"
+              type="reset"
+              color="red"
+              flat
+              class="q-ml-sm q-mt-md"
+            />
+          </div>
+        </q-card-section>
+      </q-card>
+
+      <div>
+        <q-card class="q-mt-lg">
+          <q-tabs
+            v-model="tab"
+            dense
+            class="text-grey"
+            active-color="primary"
+            indicator-color="primary"
+            align="left"
+          >
+            <q-tab name="simple" label="Simple Report" />
+            <q-tab name="detailed" label="Detailed Report" />
+            <q-tab name="diagnostic" label="Diagnostic Tree" />
+            <q-tab name="etsi" label="ETSI Validation Report" />
+          </q-tabs>
+
+          <q-separator />
+
+          <q-tab-panels v-model="tab" animated>
+            <q-tab-panel name="simple">
+              <div class="text-h6">Simple Report</div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </q-tab-panel>
+
+            <q-tab-panel name="detailed">
+              <div class="text-h6">Detailed Report</div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </q-tab-panel>
+
+            <q-tab-panel name="diagnostic">
+              <div class="text-h6">Diagnostic Tree</div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </q-tab-panel>
+
+            <q-tab-panel name="etsi">
+              <div class="text-h6">ETSI Validation Report</div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </q-tab-panel>
+          </q-tab-panels>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+
+  data() {
+    return {
+      name: null,
+      age: null,
+      model: null,
+      accept: false,
+      tab: "simple"
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      if (this.accept !== true) {
+        this.$q.notify({
+          color: "red-5",
+          textColor: "white",
+          icon: "warning",
+          message: "You need to accept the license and terms first"
+        });
+      } else {
+        this.$q.notify({
+          color: "green-4",
+          textColor: "white",
+          icon: "cloud_done",
+          message: "Submitted"
+        });
+      }
+    },
+
+    onReset() {
+      this.name = null;
+      this.age = null;
+      this.accept = false;
+    }
+  }
+};
+</script>
+
+<style>
+.q-uploader__list {
+  height: 20rem;
+  background-color: #fafafa;
+}
+</style>
